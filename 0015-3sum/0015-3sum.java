a16a4fd4d3c6d2b73cf5ffcf94317618993c809d
vikashini <vikamadhu07@gmail.com>
2025-08-23 11:30:27 +0530

    Time: 32 ms (40.35%), Space: 51.3 MB (83.56%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,74 +3,34 @@ import java.util.*;
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-
-        List<Integer> neg = new ArrayList<>();
-        List<Integer> pos = new ArrayList<>();
-        List<Integer> zeros = new ArrayList<>();
-
-        for (int num : nums) {
-            if (num < 0) neg.add(num);
-            else if (num > 0) pos.add(num);
-            else zeros.add(num);
-        }
-
-        Set<List<Integer>> result = new HashSet<>();
-
-        // Case 1: Three zeros
-        if (zeros.size() >= 3) {
-            result.add(Arrays.asList(0, 0, 0));
-        }
-
-        // Case 2: Zero with one positive and one negative
-        if (!zeros.isEmpty()) {
-            Set<Integer> negSet = new HashSet<>(neg);
-            for (int p : pos) {
-                if (negSet.contains(-p)) {
-                    result.add(Arrays.asList(-p, 0, p));
+        List<List<Integer>> result = new ArrayList<>();
+        
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
+            int left = i + 1;
+            int right = nums.length - 1;
+            
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    
+                
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
                 }
             }
         }
-
-        // Case 3: Two negatives + one positive
-        Map<Integer, List<int[]>> negPairs = new HashMap<>();
-        for (int i = 0; i < neg.size(); i++) {
-            for (int j = i + 1; j < neg.size(); j++) {
-                int sum = neg.get(i) + neg.get(j);
-                negPairs.computeIfAbsent(sum, k -> new ArrayList<>()).add(new int[]{neg.get(i), neg.get(j)});
-            }
-        }
-
-        for (int p : pos) {
-            int target = -p;
-            if (negPairs.containsKey(target)) {
-                for (int[] pair : negPairs.get(target)) {
-                    List<Integer> triplet = Arrays.asList(pair[0], pair[1], p);
-                    Collections.sort(triplet);
-                    result.add(triplet);
-                }
-            }
-        }
-
-        // Case 4: Two positives + one negative
-        Map<Integer, List<int[]>> posPairs = new HashMap<>();
-        for (int i = 0; i < pos.size(); i++) {
-            for (int j = i + 1; j < pos.size(); j++) {
-                int sum = pos.get(i) + pos.get(j);
-                posPairs.computeIfAbsent(sum, k -> new ArrayList<>()).add(new int[]{pos.get(i), pos.get(j)});
-            }
-        }
-
-        for (int n : neg) {
-            int target = -n;
-            if (posPairs.containsKey(target)) {
-                for (int[] pair : posPairs.get(target)) {
-                    List<Integer> triplet = Arrays.asList(n, pair[0], pair[1]);
-                    Collections.sort(triplet);
-                    result.add(triplet);
-                }
-            }
-        }
-
-        return new ArrayList<>(result);
+        
+        return result;
     }
 }
