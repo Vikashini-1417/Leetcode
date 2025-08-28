7cdec65e779d8fcc43c5d3a171b006e11fa7a74c
Rajavikashini Thandapani <vikamadhu07@gmail.com>
2025-08-28 12:12:38 +0530

    Time: 14 ms (71.39%), Space: 43 MB (88.61%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,59 +1,31 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-         byte[] count = new byte[2001];
-        for (int i = 0; i < nums.length; i++) {
-            count[nums[i]+1000]++;
-        }
-
-        for(int k=0,i=0; k< count.length;k++){
-            while (count[k] > 0){
-                nums[i] = k - 1000;
-                i++;
-                count[k]--;
-            }
-        }
-
-        int low = 0, high = nums.length-1;
-        while (low+1 < high -1 &&nums[low+1]+nums[high]+nums[high-1]< target){
-            low ++;
-        }
-
-        while ( high-1> low +1&& nums[low]+nums[low+1]+nums[high-1] > target){
-            high --;
-        }
-
-        int closet = Integer.MAX_VALUE;
-        int closestresult = 0;
-        for(int i = low; i < high-1; i++) {
-
-            int k = i+1;
-            int j = high;
-            while(k < j) {
-                int total = nums[i] + nums[k] + nums[j];
-                if (total > target) {
-                    j--;
-                    while (j>k&&nums[j]==nums[j+1]){
-                        j--;
-                    }
-                } else if (total < target) {
-                    k++;
-                    while (k<j&&nums[k]==nums[k-1]){
-                        k++;
-                    }
-                } else {
-                    return target;
+        Arrays.sort(nums);
+        
+        int closest_sum=nums[0]+nums[1]+nums[2];
+        for (int i=0;i<nums.length-2;i++){
+            int left=i+1;
+            int right=nums.length-1;
+           
+             while(left<right){
+                int current_sum=nums[i]+nums[left]+nums[right];
+                if(Math.abs(current_sum-target)<Math.abs(closest_sum-target)){
+                    closest_sum=current_sum;
                 }
 
-                int minus = Math.abs(total - target);
-                if (minus < closet) {
-                    closet = minus;
-                    closestresult = total;
+                if (current_sum==target){
+                    return current_sum;
                 }
-
-            }
-            while (i+1 < high-1&&nums[i] == nums[i+1]) i++;
+                else if (current_sum<target){
+                    left++;
+                }
+                else{
+                    right--;
+                }
+             }
+        
+           
         }
-
-        return closestresult;
+        return closest_sum;
     }
 }
