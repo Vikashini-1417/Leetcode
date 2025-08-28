944fcdb49619a1f4d5697a6d0d0547d74db84005
Rajavikashini Thandapani <vikamadhu07@gmail.com>
2025-08-28 19:31:56 +0530

    Time: 3 ms (99.66%), Space: 43.9 MB (62.62%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -6,19 +6,35 @@ class Solution {
 
         Arrays.sort(nums);
 
-        for (int i = 0; i < n; i++) {
-            if (i > 0 && nums[i] == nums[i-1]) continue;
-            for (int j = i + 1; j < n; j++) {
-                if (j > i + 1 && nums[j] == nums[j-1]) continue;
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicate i
+
+            // Early pruning for i
+            long min1 = (long) nums[i] + nums[i+1] + nums[i+2] + nums[i+3];
+            if (min1 > target) break; // too large
+            long max1 = (long) nums[i] + nums[n-1] + nums[n-2] + nums[n-3];
+            if (max1 < target) continue; // too small
+
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue; // skip duplicate j
+
+                // Early pruning for j
+                long min2 = (long) nums[i] + nums[j] + nums[j+1] + nums[j+2];
+                if (min2 > target) break;
+                long max2 = (long) nums[i] + nums[j] + nums[n-1] + nums[n-2];
+                if (max2 < target) continue;
+
                 int k = j + 1, l = n - 1;
+
                 while (k < l) {
                     long sum = (long) nums[i] + nums[j] + nums[k] + nums[l];
+
                     if (sum == target) {
                         output.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
                         k++;
                         l--;
-                        while (k < l && nums[k] == nums[k-1]) k++;
-                        while (k < l && nums[l] == nums[l+1]) l--;
+                        while (k < l && nums[k] == nums[k-1]) k++;  // skip duplicate k
+                        while (k < l && nums[l] == nums[l+1]) l--;  // skip duplicate l
                     } else if (sum < target) {
                         k++;
                     } else {
@@ -27,6 +43,7 @@ class Solution {
                 }
             }
         }
+
         return output;
     }
 }
