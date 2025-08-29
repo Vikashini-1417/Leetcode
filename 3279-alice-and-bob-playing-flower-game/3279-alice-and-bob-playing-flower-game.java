ae8d376ba809df6e352b2650f651aee8b42e3056
Rajavikashini Thandapani <vikamadhu07@gmail.com>
2025-08-29 10:30:02 +0530

    Time: 0 ms (100%), Space: 40.6 MB (73.13%) - LeetHub

---
+++ 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long flowerGame(int n, int m) {
+       
+      long oddn=(n+1)/2;
+      long evenn=n/2; 
+
+      long oddm=(m+1)/2;
+      long evenm=m/2; 
+
+      return (oddn*evenm+oddm*evenn);
+    }
+}
